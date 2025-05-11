0210f91588b65f0ef0bf30d213eec610c7a5c0bc
Ziyad <24983719+zshehri@users.noreply.github.com>
2025-05-11 03:36:46 +0000

    testing streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new & testing Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.write(
+    "More tests to come!"
 )
